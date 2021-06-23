87958c1a12fc7cddd1228314de3eab111833c083
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-06-23 00:16:47 -0400

    Added working backend implementation to AddPokemon.js

---
+++ src/backend/index.js
@@ -0,0 +1,42 @@
+let express = require('express')
+let {Pool} = require('pg')
+let cors = require('cors')
+let bodyParser = require('body-parser')
+
+let app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const pool = new Pool({
+    user:"cruzgonzalez",
+    host:"localhost",
+    database:"pokemon",
+    password:"Password",
+    port:"5432"
+});
+
+
+//used to add a pokemon to DB
+app.post('/addPokemon', (req, res) => {
+    const pokeName = req.body.pokeName;
+    const pokeType = req.body.pokeType;
+    const pokeMove = req.body.pokeMove;
+
+    pool.query(
+        "INSERT INTO pokemon(pokename, type, move) VALUES ($1, $2, $3)",
+        [pokeName, pokeType, pokeMove],
+        (err, result) => {
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.send(result)
+            }
+        }
+
+    )
+})
+
+app.listen(5000, () => {
+    console.log("Your app is listening on port 5000")
+})
